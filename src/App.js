1f682aef57c845b766eae4b344c78c5bf1e8df8f
Teddy <teddymczieuwa@gmail.com>
2021-05-18 21:43:10 +0100

    infinite scroll feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import {useState, useRef, useCallback} from 'react'
 import './App.css';
+import useBookSearch from './useBookSearch'
 
 function App() {
+  const [query, setQuery] = useState('')
+  const [pageNumber, setPageNumber] = useState(1)
+  const {loading, error, books, hasMore} = useBookSearch(query, pageNumber)
+
+  const observer = useRef()
+  const lastBookElementRef = useCallback(node => {
+    if(loading) return
+    if(observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      if(entries[0].isIntersecting && hasMore) {
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+      }
+    })
+    if(node) observer.current.observe(node)
+  }, [loading, hasMore])
+
+  function handleSearch(e) {
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input value={query} type="text" onChange={handleSearch}/>
+      {books.map((book, index) => {
+        if(books.length === index + 1) return <div ref={lastBookElementRef} key={book}>{book}</div>
+        return <div key={book}>{book}</div>
+      })}
+
+      {loading && <div>Loading...</div>}
+    </>
   );
 }
 
